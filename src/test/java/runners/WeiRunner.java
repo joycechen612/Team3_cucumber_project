5ad51491f0a62ae65528d88b20a18f4ecb59d3f0
Wei Wang <weiwang0521@gmail.com>
2023-03-13 15:26:05 -0400

    format code

--- src/test/java/runners/WeiRunner.java
+++ src/test/java/runners/WeiRunner.java
@@ -16,8 +16,6 @@ import org.junit.platform.suite.api.Suite;
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/html-report/index.html")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value ="uiStepDefinitions, hooks")
 @ConfigurationParameter(key =EXECUTION_DRY_RUN_PROPERTY_NAME, value="false")
-//@ConfigurationParameter(key=FILTER_TAGS_PROPERTY_NAME,value="@Luma")
-
 @ConfigurationParameter(key= FILTER_TAGS_PROPERTY_NAME,value="@Luma")
 
 
--- src/test/resources/features/LumaSearchFunction.feature
+++ src/test/resources/features/LumaSearchFunction.feature
@@ -1,9 +1,9 @@
 @Luma
 Feature: Search button function
+
   @ui
-  Scenario: Happy Path 
-  Given Navigate to Luma Homepage
-  Then user Enter search information "top" 	
-  And click the search button	
-  Then the search reasult "top" should be on the page
-  
+  Scenario: Happy Path
+    Given Navigate to Luma Homepage
+    Then user Enter search information "top"
+    And click the search button
+    Then the search reasult "top" should be on the page
